6da123c96d6292ed6b87abdb81ecc2b15878118c
arti <artiyjana@yandex.ru>
2021-03-14 14:06:50 +0300

    added new questions

--- main.py
+++ main.py
@@ -2,25 +2,25 @@ import telebot
 from telebot import types
 import json
 import emoji
+from random import choice
 
 bot = telebot.TeleBot('')
 file_info = json.load(open('info.json', encoding='utf-8'))
 questions = file_info['questions']
 number_quiz = 0
-answer_exict = False
-start_chat = False
+answer_exict, start_chat = False, False
 count = []
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     global answer_exict, number_quiz, count
+    transition = ['Попробуй ещё', 'Неверно, увы', 'Не правильно', 'Подумай ещё', 'Нет, не так']
     if int(call.data.split(';')[1]) == number_quiz:
         correct_answer = file_info["answer"][str(number_quiz)][1]
         if correct_answer == int(call.data.split(';')[0]):
             answer_to_user = file_info["answer"][str(number_quiz)][0]
-            bot.send_message(call.message.chat.id,
-                             'Aбсолютно верно! ' + answer_to_user)
+            bot.send_message(call.message.chat.id, answer_to_user)
             answer_exict = True
             if len(count) != number_quiz + 1:
                 count.append(1)
@@ -28,7 +28,7 @@ def callback_worker(call):
         else:
             if len(count) != number_quiz + 1:
                 count.append(0)
-            bot.send_message(call.message.chat.id, 'Что-то не то, попробуй снова')
+            bot.send_message(call.message.chat.id, choice(transition))
 
 
 def make_key(question, num):
@@ -42,21 +42,24 @@ def make_key(question, num):
 def quiz(message):
     global answer_exict
     global number_quiz, count, start_chat
-    bot.send_message(message.from_user.id, "Прекрасно! Начнём с простого")
+    bot.send_message(message.from_user.id, "Начнём с простого")
+    transition = ['Следующий вопрос:', 'Твой следующий вопрос:', 'Новый вопрос:', 'Лови следующий вопрос:']
     for i in range(len(questions)):
+        if i != 0:
+            bot.send_message(message.from_user.id, choice(transition))
         file_info["number_quiz"] = i
         keyboard = make_key(questions[list(questions.keys())[i]], i)
         bot.send_message(message.from_user.id, list(questions.keys())[i], reply_markup=keyboard)
         while not answer_exict:
             continue
         answer_exict = False
-    while len(count) != 2:
+    while len(count) != 4:
         continue
     if len(count) == len(questions):
-        bot.send_message(message.from_user.id, "Молодец, прошёл квиз! " + emoji.emojize(":brain:"))
-        bot.send_message(message.from_user.id, f'Вы правильно ответили на {sum(count)}/2')
-        number_quiz, count = 0, []
-        start_chat = False
+        bot.send_message(message.from_user.id, f"Это был последний вопрос, молодец! {emoji.emojize(':brain: ')}\n"
+                                               f"Спасибо за отличную игру")
+        bot.send_message(message.from_user.id, f'Вы правильно ответили на {sum(count)}/4')
+        number_quiz, count, start_chat = 0, [], False
 
 
 def generate_keyboard(*answer):
@@ -74,15 +77,16 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, "Вы не закончили прошлый квиз")
         return
     elif message.text == "/start":
-        bot.send_message(message.from_user.id, f"Привет, {message.from_user.first_name}! \n\n" 
+        bot.send_message(message.from_user.id, f"Привет, {message.from_user.first_name}! \n\n"
                                                f"Это квиз по интересным фактам из сферы IT 💡💡💡 \n"
                                                f"Всё что тебе понадобится для прохождения - смартфон 📱")
         keyboard = generate_keyboard('Да', 'Нет')
         bot.send_message(message.from_user.id, "Готов начать?", reply_markup=keyboard)
     elif message.text == 'Да' and not start_chat:
+        bot.send_message(message.from_user.id, "Отлично!", reply_markup=types.ReplyKeyboardRemove())
         start_chat = True
         quiz(message)
-    elif message.text == 'Нет':
+    elif message.text == 'Нет' and not start_chat:
         bot.send_photo(message.from_user.id, open('img/sad.jpg', 'rb'))
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "Чтобы начать - напиши: /start")
